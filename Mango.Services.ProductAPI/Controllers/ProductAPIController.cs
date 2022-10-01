1766478e53f5e6d04030e8956a946f759ef46326
bonkhead44 <reazshovon44@gmail.com>
2022-10-01 17:47:45 +0600

    product controller complete

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,115 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Security.AccessControl;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private readonly IProductRepository _iProductRepository;
+        public ProductAPIController(IProductRepository iProductRepository)
+        {
+            _iProductRepository = iProductRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _iProductRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() {
+                    ex.ToString()
+                };
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int productId)
+        {
+            try
+            {
+                ProductDto productDto = await _iProductRepository.GetProductById(productId);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() {
+                    ex.ToString()
+                };
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<object> Delete(int productId)
+        {
+            try
+            {
+                bool isSuccess = await _iProductRepository.DeleteProduct(productId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() {
+                    ex.ToString()
+                };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto data = await _iProductRepository.CreateUpdateProduct(productDto);
+                _response.Result = data;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() {
+                    ex.ToString()
+                };
+            }
+            return _response;
+        }
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto data = await _iProductRepository.CreateUpdateProduct(productDto);
+                _response.Result = data;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() {
+                    ex.ToString()
+                };
+            }
+            return _response;
+        }
+
+    }
+}
--- Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
