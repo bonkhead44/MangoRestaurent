ffd56ede4ca388c2fe579aed3fc510ddfa5f4fb1
bonkhead44 <reazshovon44@gmail.com>
2022-10-01 15:37:04 +0600

    Fill added, Dto added

---
+++ Mango.Services.ProductAPI/Models/Dtos/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Mango.Services.ProductAPI.Models.Dtos
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DispalyMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
