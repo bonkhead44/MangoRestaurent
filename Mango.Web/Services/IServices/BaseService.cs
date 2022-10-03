70a2acb4cb3c41c6401283cec93f4138ad8cee33
bonkhead44 <reazshovon44@gmail.com>
2022-10-03 19:47:13 +0600

    Base Service add

---
+++ Mango.Web/Services/IServices/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
