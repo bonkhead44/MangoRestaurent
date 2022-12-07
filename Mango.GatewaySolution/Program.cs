11a93882a4ceb858232ad1cdffe427f8e680d1f9
bonkhead44 <reazshovon44@gmail.com>
2022-12-08 00:55:13 +0600

    18-Ocelot

---
+++ Mango.GatewaySolution/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot();
+
+app.Run();
